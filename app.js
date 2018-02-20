0dd7f5de34a2f6ef1628acfba2d297f5cce72851
Luke Prosser <lukesprosser@gmail.com>
2018-02-20 14:50:55 +0000

    Add mLab

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://lukeprosser:password@ds243418.mlab.com:43418/yelpcampdb");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
